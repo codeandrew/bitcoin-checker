c317646f44bf3466067fbf8f0b8585497f084d56
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-08-14 23:02:14 +0800

    Upgrade error handling, data structures and content of email_server.py

--- email_server.py
+++ email_server.py
@@ -14,23 +14,30 @@ to_addr = email_to
 
 def main(message,buy,sell):
 
-	subject = 'Bitcoin Price, Buy:{} Sell:{}'.format(buy, sell) 
-	msg = MIMEMultipart()
-	msg['From'] = from_addr
-	msg['To'] = to_addr
-	msg['Subject'] = subject
+    subject = 'Bitcoin Price, Buy:%s Sell:%s' % (buy, sell)
+    print subject
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = subject
 
-	body = message
-	msg.attach(MIMEText(body, 'plain'))
-	
-	try:
-		server = smtplib.SMTP('smtp.gmail.com', 587)
-		server.starttls()
-		server.login(from_addr, password)
-		text = msg.as_string()
-		server.sendmail(from_addr, to_addr, text)
-		print "[*] Email Succesfully Sent!"
-		server.quit()
-	except:
-		print "[!!] Error in Sending email"
 
+    body = message
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_addr, password)
+        text = msg.as_string()
+        server.sendmail(from_addr, to_addr, text)
+        print "[*] Email Succesfully Sent!"
+        server.quit()
+    except Exception as e:
+        print "[!!] Error in Sending email", e
+
+
+if __name__ == '__main__':
+    print 'email_server is main file'
+    message = "Test Message"
+    main(message, "none", "none")
