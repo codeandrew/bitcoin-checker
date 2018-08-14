24240cef69247ed52e38baa0bc964075c72c2dd8
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-08-14 20:23:14 +0800

    Put script in function in bitcoin_fetcher

--- bitcoin_fetcher.py
+++ bitcoin_fetcher.py
@@ -2,23 +2,31 @@
 import requests
 import json
 
-url = "https://quote.coins.ph/v1/markets/BTC-PHP"
+def request():
+	url = "https://quote.coins.ph/v1/markets/BTC-PHP"
 
-headers = {
-    'Cache-Control': "no-cache",
-    'Postman-Token': "3f791a1b-c1ce-46e2-98ee-6cd4591fcd35"
-    }
+	headers = {
+	    'Cache-Control': "no-cache",
+	    'Postman-Token': "3f791a1b-c1ce-46e2-98ee-6cd4591fcd35"
+	    }
 
-response = requests.request("GET", url, headers=headers)
+	response = requests.request("GET", url, headers=headers)
 
-print response.text
+	print response.text
 
-data = json.loads(response.text)
-market = data['market']
-buy = market.get('ask')
-sell = market.get('bid')
+	data = json.loads(response.text)
+	market = data['market']
+	buy = market.get('ask')
+	sell = market.get('bid')
 
-print "Buy bitcoin at:", buy
-print "Sell bitcoin at:",  sell
+	print "Buy bitcoin at:", buy
+	print "Sell bitcoin at:",  sell
 
+	bitcoin = {
+		'buy' : buy,
+		'sell' : sell
+		}
 
+	return bitcoin
+
+request()
