cb35be8396b8a0af938e6af1eefa65b0dc661de2
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-08-14 23:00:20 +0800

    Organize config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-import os 
+import os
 #print os.environ
 
-pwd = os.getenv('TOOLS', os.getcwd() )  
+pwd = os.getenv('TOOLS', os.getcwd())
 
 def list():
-	for item, value in os.environ.items():
-	    print('{}: {}'.format(item, value))
+    for item, value in os.environ.items():
+        print('{}: {}'.format(item, value))
 
-	for i, j in os.environ.items():
-	    print(i, j)
+    for i, j in os.environ.items():
+        print(i, j)
 
 
 def execute():
-	env = open( pwd + "/.env", "r")
+    env = open( pwd + "/.env", "r")
 
-	print env.read()
-	print type(env.read())
+    print env.read()
+    print type(env.read())
 
 execute()
 
