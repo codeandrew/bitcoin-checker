354a0a69a9119f5f717fa75818b695746b9af8f0
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-08-15 18:37:22 +0800

    Create mailManager method and import Datetime in main.py

--- main.py
+++ main.py
@@ -2,15 +2,38 @@
 import config
 import bitcoin_fetcher
 import email_server
+import datetime
+
+time = datetime.datetime.now()
+
+def mailManager(time, buy, sell):
+    buyBreakpoint = buy % 100000 == 0
+    sellBreakpoint = sell % 100000 == 0
+
+    if buyBreakpoint:
+        subject = "[Buy] Bitcoin price at %s " % ( buy )
+        message = "%s \r\nYou can now buy bitcoins at %s." % ( time , buy )
+
+    if sellBreakpoint:
+        subject = "[Sell] Bitcoin price at %s " % ( sell )
+        message = "%s \r\nYou can now sell bitcoins at %s." % ( time , sell )
+
+
+    data = {
+        'subject' : subject,
+        'message' : message,
+    }
+
 
 def main():
+    print time
     config.execute()
 
     bc = bitcoin_fetcher.request()
     buy = bc.get('buy')
     sell = bc.get('sell')
 
-    message = "Buy Bitcoins at %s. \r\nSell Bitcoins at %s " % ( buy, sell )
+    message = "%s \r\nBuy Bitcoins at %s. \r\nSell Bitcoins at %s " % (time, buy, sell )
 
     email_server.main(message, buy, sell)
     print "[*] Program finished."
